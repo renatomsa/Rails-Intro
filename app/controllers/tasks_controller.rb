67655fa135bbdccf47cf43e3a48e1042ba0d9e2b
Renato Serrano <renatom.sandrade@gmail.com>
2024-04-08 16:08:29 -0300

    CHORE[Feature/todo-lists-tasks]: Adding tasks to to-do list

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,18 @@
 class TasksController < ApplicationController
+
+  def create
+    @task = Task.create(task_params)
+
+    if @task.save
+      redirect_to todo_path(@task.todo_id)
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :done, :todo_id)
+  end
 end
